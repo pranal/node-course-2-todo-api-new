abeabc5713acbe992f33a809eadf1ab4552c2d6c
Pranal <pranaldesai@gmail.com>
2018-03-11 13:05:35 +0530

    Add tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -303,6 +303,25 @@ it('should clear comletedAt when todo is not completed',(done)=>{
   })
  });
 
+ describe('DELETE /users/me/token',()=>{
+  
+    it('should remove auth token on logout',(done)=>{
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth',users[0].tokens[0].token)
+    .expect(200)
+    .end((err,res)=>{
+      if(err){
+          return done(err);
+      }
+      User.findById(users[0]._id).then((user)=>{
+     expect(user.tokens.length).toBe(0);
+     done();
+      }).catch((e) =>done(e));
+    });
+    });
+ });
+
 
 
   
